1fd8612b7ca0c143b9e35a24e80d113ec967a58d
Elioenai Pinheiro Ferrari <elioenai.ferrari@ifood.com.br>
2024-10-21 09:59:35 -0300

    go client

--- example/go/go.mod
+++ example/go/go.mod
@@ -1,3 +1,7 @@
 module github.com/ElioenaiFerrari/go-example
 
 go 1.22.0
+
+require github.com/ElioenaiFerrari/rhino v0.0.0-20241021123703-4494d201c986
+
+require google.golang.org/protobuf v1.35.1 // indirect
--- example/go/main.go
+++ example/go/main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	pb "github.com/ElioenaiFerrari/rhino/gen/go"
+)
+
 func main() {
+	publishRequest := pb.PublishRequest{
+		Topic:      "topic",
+		Data:       []byte("data"),
+		Ttl:        1000,
+		Persistent: true,
+	}
+
+	subscriptionRequest := pb.SubscriptionRequest{
+		Topic: "topic",
+	}
+
+	rhinov1connect.Publish(&publishRequest)
+
 }
